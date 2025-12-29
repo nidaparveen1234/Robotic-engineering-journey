c50d35517d1d2e7700488da7267acdef99f64fc7
nidaparveen1234 <nidavattathuru@gmail.com>
2025-12-29 19:02:35 +0530

    Day 8: List traversal and basic transformations

---
+++ Day8_listtraversal_andtransfromation.py
@@ -0,0 +1,31 @@
+#Numbers =[1, 34,2434,24,2634,24,26,3934,96,39]
+#for i in Numbers:
+ #   print(i, end=" ") 
+    
+#print(Numbers[1])
+#print(Numbers[-1])
+
+listA=[1,2,3,4,5,6,7,25]
+listB=[]
+for i in listA:
+    doubled = i*i
+    listB.append(doubled)
+print(listB)
+
+# output
+# [1, 4, 9, 16, 25, 36, 49, 625]
+list=[1,2,34,5,6,778,97687,684,5676,765,4,3,333,2,100]
+for i in list:
+    print(i, end=" ")
+
+print() #i have same line error it is to clear that 
+# 1 2 34 5 6 778 97687 684 5676 765 4 3 333 2 100 1
+# 100
+print(list[0])
+print(list[-1])
+
+
+# output
+# 1 2 34 5 6 778 97687 684 5676 765 4 3 333 2 100 
+# 1
+# 100
