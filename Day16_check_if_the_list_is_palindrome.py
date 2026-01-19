07d565e28bf34b44357ab96c21394b02ac3590aa
nidaparveen1234 <nidavattathuru@gmail.com>
2026-01-19 19:50:08 +0530

    Update palindrome check logic in the list
    Fix palindrome check logic to compare elements correctly.

--- Day16_check_if_the_list_is_palindrome.py
+++ Day16_check_if_the_list_is_palindrome.py
@@ -1,14 +1,18 @@
-num_list = [1,2,3,45,3,45,3,2,1]
+num_list = [7,7,1,3,4,2,2,4,2]
 start = 0
 end = len(num_list) - 1
 flag = 0
-while start < end:
+while num_list[start] < num_list[end]:
      if num_list[start] == num_list[end]:
           flag = 1
-          start+=1
-          end-=1
+          num_list[start]+=1
+          num_list[end]-=1
     
 if flag==1:
     print("this is  palindrome")
 else:
-    print("this is not palindrome") ‎<This message was edited>
+    print("this is not palindrome")
+
+#Output
+#num_list = [1,2,3,45,3,45,3,2,1]===this is palindrome
+#num_list = [1,2,3,45,3,3,2,1]===this is not palindrome
