f456b83d78af1bda74dfc6cf97222d7f2fb42665
nidaparveen1234 <nidavattathuru@gmail.com>
2026-01-19 19:57:15 +0530

    Update Day16_check_if_the_list_is_palindrome.py

--- Day16_check_if_the_list_is_palindrome.py
+++ Day16_check_if_the_list_is_palindrome.py
@@ -2,16 +2,17 @@ num_list = [7,7,1,3,4,2,2,4,2]
 start = 0
 end = len(num_list) - 1
 flag = 0
-while num_list[start] < num_list[end]:
-     if num_list[start] == num_list[end]:
+while start < end:
+     if num_list[start] != num_list[end]:
           flag = 1
-          num_list[start]+=1
-          num_list[end]-=1
+          break
+          start+=1
+          end-=1
     
 if flag==1:
-    print("this is  palindrome")
-else:
     print("this is not palindrome")
+else:
+    print("this is  palindrome")
 
 #Output
 #num_list = [1,2,3,45,3,45,3,2,1]===this is palindrome
