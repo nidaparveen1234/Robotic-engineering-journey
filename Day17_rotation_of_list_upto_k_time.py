b1fd2268077caea9399e5625473e84f8424f445e
nidaparveen1234 <nidavattathuru@gmail.com>
2026-01-22 15:28:21 +0530

    Refactor list rotation logic for num_list
    Updated rotation logic for num_list and adjusted k value handling.

--- Day17_rotation_of_list_upto_k_time.py
+++ Day17_rotation_of_list_upto_k_time.py
@@ -1,15 +1,17 @@
-lst = ['A', 'B', 'C', 'D', 'E']
-k = 2
-
+num_list = [1,2,3,4,5]
 start = 0
-end = len(lst) - 1
-
+end = len(num_list) -1
+k = 3
 for r in range(k):
-    temp = lst[end]            
+    temp = num_list[end]
+    for i in range(end,start,-1):
+        num_list[i] = num_list[i-1]
+        i-=1
+    num_list[start] = temp
+print(num_list)
 
-    for i in range(end, start, -1):
-        lst[i] = lst[i-1]
-
-    lst[start] = temp            
-
-print(lst)
+# output 
+# when we are not considering k. this is output [5, 1, 2, 3, 4] but with k = [4, 1, 1, 2, 3]
+# ohh now isee my confusion num_list[start] = temp was outside outer loop at first 
+# so temp value was not getting stored now it is inside so eaxh temp vallue is getting stored
+#when k = 2 , [4, 5, 1, 2, 3] when k = 3 , [3, 4, 5, 1, 2]
