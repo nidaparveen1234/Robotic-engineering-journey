ea3270f3077134536ca7238018758d6bcdd64415
nidaparveen1234 <nidavattathuru@gmail.com>
2026-02-06 20:11:26 +0530

    Update Day24_count_duplicates_in_list.py

--- Day24_count_duplicates_in_list.py
+++ Day24_count_duplicates_in_list.py
@@ -1,14 +1,24 @@
-list = [1,2,3,4,4,32,1,3,4,5,32,1,1]
-result =[]
-count = 0
-for value in list:
-    for item in result:
-        if item[0] in result:
-            item[1]+=1
-        else:
-            result.append([value][1])
+num_list =[1,2,34,5,3,4,3,2,2,1]
+result_list =[]
+for value in num_list:
+    Found = False
 
-for key in result:
+    for item in result_list:
+        if item[0] == value:
+            item[1]+=1
+            Found = True
+    if not Found:
+        result_list.append([value,1])
+
+for key in result_list:
     print(key[0],"-",key[1])
 
-    #error
+#output == this question i worked so hard for me to get and  i got almost everything except for nested lists side
+# and not found but i got some help from chatgpt and i was aable to reach here and i have more to learn in my journey 
+# to be a better engineer
+# 1 - 2
+# 2 - 3
+# 34 - 1
+# 5 - 1
+# 3 - 2
+# 4 - 1
